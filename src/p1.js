2be4eed03f3a085f4c49d784fb69346096e03ace
Ewelina <ewe.krysiak@gmail.com>
2019-10-27 11:54:19 +0100

    Json and first example

--- src/p1.js
+++ src/p1.js
@@ -1,8 +1,8 @@
 function p1(a){
     //[1,2,10] =>{(1), (2), (10)]
 
-    return a.map(x => `(${x}`).join (' ')
+    return a.map(x => `${x}`).join (' ')
 
 }
 
-console.log(p1([1, 20, 10])
+console.log(p1([1, 20, 10]));
